ebd18f3867457d5bc379eae869756f7bf40c1ef5
Pedro Passos <100157780+pedropassos22@users.noreply.github.com>
2022-07-25 15:52:52 -0300

    as

--- exercicios/script_exercicio_um.sql
+++ exercicios/script_exercicio_um.sql
@@ -12,15 +12,15 @@ create table LIVROS (
 );
 
 INSERT INTO LIVROS
-(NOME, AUTOR, SEXOAUTOR, NUMEROPAGINAS, EDITORA, VALORLIVRO, ESTADOEDITORA, ANOLIVRO)
+(NOME, AUTOR, SEXOAUTOR, NUMEROPAGINAS, EDITORA, VALORLIVRO, UF, ANOLIVRO)
 VALUES
-('Cavaleiro Real', 'Ana Claudia','F','465', 'Atlas', 49, 'RJ', '2009'),
+('Cavaleiro Real', 'Ana Claudia','F','465', 'Atlas', 49.9, 'RJ', '2009'),
 ('SQL para leigos', 'João Nunes','M','450', 'Addison', 98, 'SP', '2018'),
-('Receitas Caseiras', 'Celia Tavares','F','210', 'Atlas', 45, 'RJ', '2018'),
-('Pessoas Efetivas', 'Eduardo Santos','M','390', 'Beta', 78, 'RJ', '2018'),
-('Habitos Saudáveis', 'Eduardo Santos','M','630', 'Beta', 150, 'RJ', '2018'),
-('A Casa Marrom', 'Hermes Macedo','M','250', 'Bubba', 60, 'MG', '2018'),
-('Estacio Querido', 'Geraldo Francisco','M','310', 'Insignia', 100, 'ES', '2018'),
-('Pra sempre amigas', 'Leda Silva','F','410', 'Insignia', 78, 'ES', '2018'),
-('Copas Inesqueciveis', 'Marco Alcantara','M','200', 'Larson', 130, 'RS', '2018'),
-('O poder da mente', 'Clara Mafra','F','120', 'Continental', 56, 'SP', '2018');
+('Receitas Caseiras', 'Celia Tavares','F','210', 'Atlas', 45, 'RJ', '2008'),
+('Pessoas Efetivas', 'Eduardo Santos','M','390', 'Beta', 78.09, 'RJ', '2018'),
+('Habitos Saudáveis', 'Eduardo Santos','M','630', 'Beta', 150.98, 'RJ', '2019'),
+('A Casa Marrom', 'Hermes Macedo','M','250', 'Bubba', 60, 'MG', '2016'),
+('Estacio Querido', 'Geraldo Francisco','M','310', 'Insignia', 100, 'ES', '2015'),
+('Pra sempre amigas', 'Leda Silva','F','410', 'Insignia', 78.98, 'ES', '2011'),
+('Copas Inesqueciveis', 'Marco Alcantara','M','200', 'Larson', 130.98, 'RS', '2018'),
+('O poder da mente', 'Clara Mafra','F','120', 'Continental', 56.58, 'SP', '2017');
